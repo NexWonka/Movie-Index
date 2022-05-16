92e9ac10c144a6400588efe383e6cd87bcdcba50
NexWonka <willy77889@gmail.com>
2022-05-16 16:40:12 +0800

    UI & API CONNECT

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,28 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Movies Index</title>
+</svelte:head>
+
+<div class="container flex flex-wrap p-5 justify-between" id="nav_container">
+	<div>
+		<img src="" alt="Movies" />
+	</div>
+	<div class="flex flex-wrap gap-5" id="nav_page">
+		<a href="#">Home</a>
+		<a href="#">Categories</a>
+		<a href="#">Popular</a>
+		<a href="#">Trending</a>
+	</div>
+</div>
 <slot />
+
+<style>
+	#nav_container {
+		background: #FF8C32;
+	}
+	#nav_page{
+		color: #06113C;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	import * as axi from 'axios';
+
+	export async function load({ fetch }) {
+		const movies = await fetch('https://imdb-api.com/en/API/MostPopularMovies/k_wu3sr107');
+		const guides = await movies.json();
+        console.log(guides);
+
+		return {
+			props: {
+				guides
+			}
+		};
+	}
+</script>
+
+<script>
+	export let guides;
+</script>
+
+<div class="flex flex-wrap h-screen flex-column justify-center" id="container">
+	<div class="w-full">
+		<div class="py-10 px-10">
+			<input class="w-full h-14 px-10 rounded-full" type="text" placeholder="Search Movies..." />
+		</div>
+		<div class="grid grid-cols-10 text-white gap-5 mx-10">
+			<div>
+				<div class="flex flex-wrap bg-red-100 p-5 items-center justify-center rounded-xl">
+					{#each guides as movies}
+						<h1>{movies.items.title}</h1>
+						<p>{movies.items.year}</p>
+						<p>{movies.items.rating}</p>
+					{/each}
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	#container {
+		background: #06113c;
+	}
+</style>
