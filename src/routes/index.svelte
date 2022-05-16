b0015c0db8e080cedf4410d7e697adefe50a7157
NexWonka <willy77889@gmail.com>
2022-05-16 18:10:20 +0800

    fetch test using mock api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,37 +2,39 @@
 	import * as axi from 'axios';
 
 	export async function load({ fetch }) {
-		const movies = await fetch('https://imdb-api.com/en/API/MostPopularMovies/k_wu3sr107');
-		const guides = await movies.json();
-        console.log(guides);
+		const movies = await fetch('https://jsonplaceholder.typicode.com/photos');
+		const res = await movies.json();
 
 		return {
 			props: {
-				guides
+				res
 			}
 		};
-	}
+	};
 </script>
 
 <script>
-	export let guides;
+	export let res;
+	let search;
+
+	function searchMovies(){
+	};
 </script>
 
-<div class="flex flex-wrap h-screen flex-column justify-center" id="container">
+<div class="flex flex-wrap flex-column justify-center" id="container">
 	<div class="w-full">
-		<div class="py-10 px-10">
-			<input class="w-full h-14 px-10 rounded-full" type="text" placeholder="Search Movies..." />
+		<div class="flex flex-wrap justify-center items-center gap-5 py-10 px-10">
+			<input class="w-1/2 h-14 px-10 rounded-full" type="text" placeholder="Search Movies..." bind:value={search}/>
+			<button id="search" class="p-3 rounded-xl font-bold hover:text-white transition" type="submit" on:submit={searchMovies}>Search</button>
 		</div>
 		<div class="grid grid-cols-10 text-white gap-5 mx-10">
-			<div>
-				<div class="flex flex-wrap bg-red-100 p-5 items-center justify-center rounded-xl">
-					{#each guides as movies}
-						<h1>{movies.items.title}</h1>
-						<p>{movies.items.year}</p>
-						<p>{movies.items.rating}</p>
-					{/each}
+			{#each res as movies}
+				<div>
+					<div class="flex flex-wrap bg-red-500 p-5 items-center justify-center rounded-xl">
+						<h1>{movies.title}</h1>
+					</div>
 				</div>
-			</div>
+			{/each}
 		</div>
 	</div>
 </div>
@@ -40,5 +42,11 @@
 <style>
 	#container {
 		background: #06113c;
+	}
+	#search{
+		background: #FF8C32;
+	}
+	#search:active{
+		background: #9c510a;
 	}
 </style>
