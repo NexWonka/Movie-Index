442a4240ef081c9e70e9b28e1b60bb31bff9a252
NexWonka <willy77889@gmail.com>
2022-05-17 23:46:37 +0800

    Add clear input button & footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,7 +17,15 @@
 		<a class="hover:text-white transition" href="#">Coming Soon</a>
 	</div>
 </div>
+
 <slot />
+
+<div class="flex p-3 justify-end" id="nav_container">
+	<div class="flex gap-5 text-sm" id="nav_page">
+		<p class="italic">Made by <a href="https://github.com/NexWonka" class="not-italic font-bold">NexWonka</a></p>
+		<p class="italic">Using <a href="https://imdb-api.com/" class="not-italic font-bold">IMDb API</a></p>
+	</div>
+</div>
 
 <style>
 	#nav_container {
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
 	import axios from 'axios';
 
 	let query = '';
+	let click = 0;
 	let resQuery = writable([]);
 
 	onMount(async () => {
@@ -14,35 +15,78 @@
 	async function search() {
 		const response = await axios.get(`https://imdb-api.com/en/API/SearchTitle/k_wu3sr107/${query}`);
 		const data = response.data.results;
+		click++;
 		return resQuery.set(data);
+	}
+
+	function clear() {
+		query = '';
+		click = 0;
 	}
 </script>
 
 <div class="flex flex-wrap flex-column justify-center py-10" id="container">
-	<div class="w-full">
+	<div class="w-full mx-10">
 		<h1 class="font-bold text-8xl text-white text-center">MOVIE INDEX</h1>
-		<div class="flex flex-wrap justify-center items-center gap-5 py-10 px-10">
+		<div class="flex flex-wrap justify-center items-center gap-2 py-10 px-10">
 			<input
 				class="w-1/2 h-14 px-10 rounded-full"
 				type="text"
 				placeholder="Search Movies..."
 				bind:value={query}
 			/>
-			<button
-				id="search"
-				class="p-3 rounded-xl hover:text-white transition"
-				type="submit"
-				on:click={search}>Search</button
-			>
+			{#if !click}
+				<button
+					id="search"
+					class="p-3 rounded-full hover:text-white transition"
+					type="submit"
+					on:click={search}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						stroke-width="2"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+						/>
+					</svg></button
+				>
+			{:else}
+				<button
+					id="search"
+					class="p-3 rounded-full hover:text-white transition"
+					type="submit"
+					on:click={clear}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						stroke-width="2"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg></button
+				>
+			{/if}
 		</div>
-		<div class="grid xs:grid-cols-1 sm:grid-cols-3 md:grid-cols-6 xl:grid-cols-8 gap-5 mx-10">
-			{#if query === ""}
+		<h1 class="font-bold text-3xl text-white">BOX OFFICE POPULAR</h1>
+		<p class="mb-5 text-white">―――――――――――――</p>
+		<div class="grid grid-rows-1 sm:grid-cols-3 md:grid-cols-6 xl:grid-cols-8 gap-5">
+			{#if !click}
 				{#each $datas as movies}
 					<div>
-						<div
-							class="flex flex-col bg-white p-2 items-center justify-start rounded-xl text-center"
+						<button
+							class="flex flex-col bg-white w-full p-2 items-center justify-center md:justify-start rounded-xl text-center hover:bg-gray-300 transition"
 						>
-							<img class="rounded-xl" src={movies.image} alt="" />
+							<img class="rounded-xl object-contain" src={movies.image} alt="" />
 							<h1 id="movieTitle" class="font-bold">{movies.title}</h1>
 							<p id="movieYear" class="mb-5">({movies.year})</p>
 							<p class="self-start text-xs">
@@ -50,7 +94,7 @@
 							</p>
 							<p class="self-start text-xs">Rank {movies.rank}</p>
 							<p class="self-start text-xs">Popularity {movies.rankUpDown}</p>
-						</div>
+						</button>
 					</div>
 				{:else}
 					<div class="h-screen">
@@ -60,13 +104,13 @@
 			{:else}
 				{#each $resQuery as movies}
 					<div>
-						<div
-							class="flex flex-col bg-white p-2 items-center justify-start rounded-xl text-center"
+						<button
+							class="flex flex-col h-full md:w-full bg-white p-2 items-center justify-center md:justify-start rounded-xl text-center"
 						>
 							<img class="rounded-xl" src={movies.image} alt="" />
 							<h1 id="movieTitle" class="font-bold">{movies.title}</h1>
 							<p id="movieYear" class="mb-5">{movies.description}</p>
-						</div>
+						</button>
 					</div>
 				{:else}
 					<div class="h-screen">
