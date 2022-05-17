de620cea870c088944acb583ebe804599df8ebfe
NexWonka <willy77889@gmail.com>
2022-05-17 14:16:27 +0800

    Fetch & show data from IMDb api

--- src/routes/getData.js
+++ src/routes/getData.js
@@ -4,8 +4,8 @@ import { writable } from "svelte/store";
 export const datas = writable([]);
 
 const getData = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/photos");
-    const data = response.data;
+    const response = await axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_wu3sr107");
+    const data = response.data.items;
     return datas.set(data);
 }
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,20 +7,25 @@
 	});
 </script>
 
-<div class="flex flex-wrap flex-column justify-center" id="container">
+<div class="flex flex-wrap flex-column justify-center py-10" id="container">
 	<div class="w-full">
+		<h1 class="font-bold text-8xl text-white text-center">MOVIE INDEX</h1>
 		<div class="flex flex-wrap justify-center items-center gap-5 py-10 px-10">
 			<input class="w-1/2 h-14 px-10 rounded-full" type="text" placeholder="Search Movies..." />
 			<button id="search" class="p-3 rounded-xl hover:text-white transition" type="submit"
 				>Search</button
 			>
 		</div>
-		<div class="grid grid-cols-8 text-white gap-5 mx-10">
+		<div class="grid grid-cols-8 gap-5 mx-10">
 			{#each $datas as movies}
 				<div>
-					<div class="flex flex-wrap bg-red-500 h-full p-5 items-start justify-center rounded-xl">
-						<img class="rounded" src={movies.thumbnailUrl} alt="" />
-						<h1>{movies.title}</h1>
+					<div class="flex flex-col bg-white p-2 items-center justify-start rounded-xl text-center">
+						<img class="rounded-xl" src={movies.image} alt="" />
+						<h1 id="movieTitle" class="font-bold">{movies.title}</h1>
+						<p id="movieYear" class="mb-5">({movies.year})</p>
+						<p class="self-start text-xs">Rating {movies.imDbRating} ({movies.imDbRatingCount})</p>
+						<p class="self-start text-xs">Rank {movies.rank}</p>
+						<p class="self-start text-xs">Popularity {movies.rankUpDown}</p>
 					</div>
 				</div>
 			{:else}
@@ -41,5 +46,11 @@
 	}
 	#search:active {
 		background: #9c510a;
+	}
+	#movieTitle {
+		color: #06113c;
+	}
+	#movieYear {
+		color: #06113c;
 	}
 </style>
