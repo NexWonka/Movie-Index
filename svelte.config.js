663dc0b400eab1c0d41c5d10fd2659b2a3fcc61e
NexWonka <willy77889@gmail.com>
2022-05-18 18:04:28 +0800

    reconfig svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,10 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		edge: false,
-		split: false
+		adapter: adapter({
+			edge: false,
+			split: false
+		})
 	}
 };
 
